4c16b995087ca33b83dd06f0b369d2d69e07717b
AdderEd <cuervadealaizandres@gmail.com>
2026-02-25 20:39:13 +0100

    cambiosAPP

--- src/main/java/org/palomafp/clinicaveterinaria/App.java
+++ src/main/java/org/palomafp/clinicaveterinaria/App.java
@@ -1,14 +1,60 @@
 package org.palomafp.clinicaveterinaria;
 
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Mostrar la información de la cita
+     
+        // Declaraciones
 
-        CitaDAO dao = new CitaDAO();
+        Scanner sc = new Scanner(System.in);
+        boolean bucleMenu = true;
+        int opcionesMenu;
 
-        System.out.println(dao.toString());
-        
+        // Código
+
+        do {
+            
+            System.out.println("======================================");
+            System.out.println("CLINICA VETERINARIA");
+            System.out.println("======================================");
+            System.out.println("1. Mostrar cita por indice.");
+            System.out.println("2. Mostrar cita aleatoria.");
+            System.out.println("3. Mostrar todas las citas.");
+            System.out.println("4. Salir del programa,");
+
+            opcionesMenu = sc.nextInt();
+
+            switch (opcionesMenu) {
+
+                case 1:
+                    
+                    break;
+            
+                case 2:
+
+                    break;
+
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    System.out.println("¡Hasta pronto!");
+                    bucleMenu = false;
+                    break;
+
+                default:
+
+                    System.out.println("¡ERROR! El valor introducido no es válido.");
+                    break;
+            }
+
+        } while (bucleMenu == true);
+
+        sc.close();
     }
 }
