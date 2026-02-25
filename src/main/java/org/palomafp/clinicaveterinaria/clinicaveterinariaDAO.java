368fae62d6f0f47d45a330d0e7393f92dd6a728b
AdrianStephano <adrian.alvarado2@educa.madrid.org>
2026-02-25 19:55:51 +0100

    metodos_utiles_devueltos

--- src/main/java/org/palomafp/clinicaveterinaria/clinicaveterinariaDAO.java
+++ src/main/java/org/palomafp/clinicaveterinaria/clinicaveterinariaDAO.java
@@ -11,8 +11,7 @@ public class clinicaveterinariaDAO {
     public clinicaveterinariaDAO() {
         this.citas = new ArrayList<>();
     }
-
-    // Objetos
+    // Objeto
     Clinica clinica = new Clinica(1, "Clínica Veterinaria PalomaFP", "Calle Principal 123, Madrid", "914567890");
     Veterinario veterinario = new Veterinario(1, "Dr. Carlos García", "12345678A", "612345678", "carlos@veterinaria.com", clinica);
     DuenyoMascota duenyoMascota = new DuenyoMascota(1, "Juan Pérez", "87654321B", "687654321", "juan@email.com");
@@ -20,13 +19,22 @@ public class clinicaveterinariaDAO {
     Cita cita = new Cita(1, new Date(), new Date(), veterinario, duenyoMascota);
     
     // Métodos
-    public String mostrarInformacion() {
-    return "Información de la Clínica Veterinaria:\n" +
-           clinica.toString() + "\n" +
-           "Veterinario:\n" + veterinario.toString() + "\n" +
-           "Dueño de Mascota:\n" + duenyoMascota.toString() + "\n" +
-           "Mascota:\n" + mascota.toString() + "\n" +
-           "Cita:\n" + cita.toString() + "\n";
+
+    public getCita() {
+
+        return this.citas.get(0);
+    }
+
+    public getAllCitas() {
+
+        return this.citas;
+    }
+
+    public delCita(int num) {
+
+        this.citas(num) = null;
+
+        return this.citas(num);
     }
 
 }
